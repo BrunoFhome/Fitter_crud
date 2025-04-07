c948a6bc693ed5fbc3e0ec6b8a56151d9747bbfd
BrunoFhome <brunodevjunior2022@gmail.com>
2025-04-07 16:21:43 -0300

    modificacao tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(GymWorkoutApp());
+  runApp(MaterialApp(
+    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class GymWorkoutApp extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: WorkoutScreen(),
-      debugShowCheckedModeBanner: false,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu Principal"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text(
+              "Bem Vindo ao",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "FITTER",
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(250, 70),
+                textStyle: TextStyle(fontSize: 20, color: Colors.blue),
+                foregroundColor: Colors.blue,
+              ),
+              child: Text("Sua Planilha"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    transitionDuration: Duration(milliseconds: 500),
+                    pageBuilder: (context, animation, secondaryAnimation) => WorkoutScreen(),
+                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                      return FadeTransition(
+                        opacity: animation,
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(250, 70),
+                textStyle: TextStyle(fontSize: 20, color: Colors.blue),
+                foregroundColor: Colors.blue,
+              ),
+              child: Text("Configurações"),
+              onPressed: () {
+                // nao faz nada por enquanto
+              },
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
     );
   }
 }
@@ -20,24 +87,34 @@ class WorkoutScreen extends StatefulWidget {
 }
 
 class _WorkoutScreenState extends State<WorkoutScreen> {
-  final List<String> days = ["Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabádo", "Domingo"];
+  final List<String> days = [
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sabádo",
+    "Domingo"
+  ];
   int selectedDayIndex = 0;
-  
-  final List<String> availableExercises = ["Flexões", "Agachamento", "Supino Reto", "Levantamento Terra", "Biceps Rosca", "Leg press"];
-  final Map<String, List<String>> workoutPlans = {};
 
-  // Controller to manage text input for new or edited exercises
+  final List<String> availableExercises = [
+    "Flexões",
+    "Agachamento",
+    "Supino Reto",
+    "Levantamento Terra",
+    "Biceps Rosca",
+    "Leg press"
+  ];
+  final Map<String, List<String>> workoutPlans = {};
   final TextEditingController _exerciseController = TextEditingController();
 
-  // Add a map to hold custom explanations for each exercise
   final Map<String, String> exerciseExplanations = {
-    "Flexões": "As flexões são um exercício que fortalece os músculos da parte superior do corpo, como ombros, braços e peitorais. Elas também ajudam a melhorar a estabilidade do core, a resistência e o equilíbrio. ",
-    "Agachamento": "O agachamento é um exercício de força que serve para fortalecer os músculos das pernas e do core, além de melhorar a mobilidade e o equilíbrio. ",
-    "Supino Reto": "A modalidade conhecida como supino reto, é o exercício que mais caracteriza o levantamento de peso. Definitivamente é um dos principais para quem deseja fortalecer ou ganhar massa muscular na região do peitoral.",
-    "Levantamento Terra": "Trabalhos multiarticulares, como o 'terra', são os mais eficazes para promover o ganho de força, pois possibilitam um maior incremento de carga. Além disso, há também o fato de que ele aumenta o gasto energético. ",
-    "Biceps Rosca": "A rosca direta é um exercício indicado para fortalecer e hipertrofiar os bíceps e trabalhar também outros músculos dos membros superiores ",
-    "Leg press": "O Leg Press estimula a hipertrofia dos músculos dos membros inferiores. ",
-
+    "Flexões":
+        "As flexões são um exercício que fortalece os músculos da parte superior do corpo...",
+    "Agachamento":
+        "O agachamento é um exercício de força que serve para fortalecer os músculos das pernas...",
+    // adicionar mais depois.
   };
 
   @override
@@ -51,36 +128,34 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
   void _addNewExercise() {
     showDialog(
       context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Novo Exercício"),
-          content: TextField(
-            controller: _exerciseController,
-            decoration: InputDecoration(hintText: "Digite o nome do exercício"),
+      builder: (context) => AlertDialog(
+        title: Text("Novo Exercício"),
+        content: TextField(
+          controller: _exerciseController,
+          decoration: InputDecoration(hintText: "Digite o nome do exercício"),
+        ),
+        actions: [
+          TextButton(
+            child: Text("Cancelar"),
+            onPressed: () {
+              _exerciseController.clear();
+              Navigator.of(context).pop();
+            },
           ),
-          actions: [
-            TextButton(
-              child: Text("Cancelar"),
-              onPressed: () {
-                _exerciseController.clear();
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text("Adicionar"),
-              onPressed: () {
-                if (_exerciseController.text.isNotEmpty) {
-                  setState(() {
-                    availableExercises.add(_exerciseController.text);
-                  });
-                }
-                _exerciseController.clear();
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+          TextButton(
+            child: Text("Adicionar"),
+            onPressed: () {
+              if (_exerciseController.text.isNotEmpty) {
+                setState(() {
+                  availableExercises.add(_exerciseController.text);
+                });
+              }
+              _exerciseController.clear();
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
     );
   }
 
@@ -88,46 +163,47 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
     _exerciseController.text = availableExercises[index];
     showDialog(
       context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Editar Exercício"),
-          content: TextField(
-            controller: _exerciseController,
-            decoration: InputDecoration(hintText: "Digite o novo nome"),
+      builder: (context) => AlertDialog(
+        title: Text("Editar Exercício"),
+        content: TextField(
+          controller: _exerciseController,
+          decoration: InputDecoration(hintText: "Digite o novo nome"),
+        ),
+        actions: [
+          TextButton(
+            child: Text("Cancelar"),
+            onPressed: () {
+              _exerciseController.clear();
+              Navigator.of(context).pop();
+            },
           ),
-          actions: [
-            TextButton(
-              child: Text("Cancelar"),
-              onPressed: () {
-                _exerciseController.clear();
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text("Salvar"),
-              onPressed: () {
-                if (_exerciseController.text.isNotEmpty) {
-                  setState(() {
-                    availableExercises[index] = _exerciseController.text;
-                  });
-                }
-                _exerciseController.clear();
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+          TextButton(
+            child: Text("Salvar"),
+            onPressed: () {
+              if (_exerciseController.text.isNotEmpty) {
+                setState(() {
+                  availableExercises[index] = _exerciseController.text;
+                });
+              }
+              _exerciseController.clear();
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("FITTER"),centerTitle: true,),
+      appBar: AppBar(
+        title: Text("FITTER"),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
-          // Day selection
+          
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
@@ -151,33 +227,41 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
           Expanded(
             child: Row(
               children: [
-                // LISTA DE EXERCICIOS PARA USUARIO ESCOLHER
+                
                 Expanded(
                   child: Container(
                     padding: EdgeInsets.all(8.0),
                     color: const Color.fromARGB(255, 129, 230, 255),
                     child: Column(
                       children: [
-                        Text("Exercicios Disponiveis", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        Text(
+                          "Exercicios Disponiveis",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
                         Expanded(
                           child: ListView.builder(
                             itemCount: availableExercises.length,
                             itemBuilder: (context, index) {
+                              final exerciseName = availableExercises[index];
                               return Draggable<String>(
-                                data: availableExercises[index],
+                                data: exerciseName,
                                 feedback: Material(
                                   child: Container(
                                     padding: EdgeInsets.all(8.0),
                                     color: Colors.blue,
-                                    child: Text(availableExercises[index], style: TextStyle(color: Colors.white)),
+                                    child: Text(
+                                      exerciseName,
+                                      style: TextStyle(color: Colors.white),
+                                    ),
                                   ),
                                 ),
                                 child: ListTile(
-                                  title: Text(availableExercises[index]),
+                                  title: Text(exerciseName),
                                   onTap: () {
-                                    // Use the map to retrieve a custom explanation
-                                    final exerciseName = availableExercises[index];
-                                    final explanation = exerciseExplanations[exerciseName] ?? "Nenhuma explicação fornecida.";
+                                    final explanation =
+                                        exerciseExplanations[exerciseName] ??
+                                            "Nenhuma explicação fornecida.";
                                     showDialog(
                                       context: context,
                                       builder: (context) => AlertDialog(
@@ -186,17 +270,16 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
                                         actions: [
                                           TextButton(
                                             child: Text("Fechar"),
-                                            onPressed: () => Navigator.of(context).pop(),
+                                            onPressed: () =>
+                                                Navigator.of(context).pop(),
                                           ),
                                         ],
                                       ),
                                     );
                                   },
                                   trailing: IconButton(
-                                    icon: Icon(Icons.edit, color: const Color.fromARGB(255, 0, 0, 0)),
-                                    onPressed: () {
-                                      _editExercise(index);
-                                    },
+                                    icon: Icon(Icons.edit),
+                                    onPressed: () => _editExercise(index),
                                   ),
                                 ),
                               );
@@ -207,16 +290,18 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
                     ),
                   ),
                 ),
-                // Workout Plan
+                
                 Expanded(
                   child: DragTarget<String>(
                     onAccept: (exercise) {
-                      if (workoutPlans[days[selectedDayIndex]]!.contains(exercise)) {
+                      if (workoutPlans[days[selectedDayIndex]]!
+                          .contains(exercise)) {
                         showDialog<bool>(
                           context: context,
                           builder: (context) => AlertDialog(
                             title: Text("Exercicio Duplicado"),
-                            content: Text("Este exercicio ja foi incluido, gostaria de adiciona-lo novamente?"),
+                            content: Text(
+                                "Este exercicio ja foi incluido, gostaria de adiciona-lo novamente?"),
                             actions: [
                               TextButton(
                                 child: Text("Não"),
@@ -231,16 +316,20 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
                         ).then((proceed) {
                           if (proceed == true) {
                             setState(() {
-                              workoutPlans[days[selectedDayIndex]]!.add(exercise);
+                              workoutPlans[days[selectedDayIndex]]!
+                                  .add(exercise);
                             });
                           }
                         });
-                      } else if (workoutPlans[days[selectedDayIndex]]!.length >= 8) {
+                      } else if (workoutPlans[days[selectedDayIndex]]!
+                              .length >=
+                          8) {
                         showDialog<bool>(
                           context: context,
                           builder: (context) => AlertDialog(
                             title: Text("Limite de exercícios"),
-                            content: Text("Você já adicionou 8 exercícios. Que é uma boa média diária. Deseja adicionar mais?"),
+                            content: Text(
+                                "Você já adicionou 8 exercícios. Que é uma boa média diária. Deseja adicionar mais?"),
                             actions: [
                               TextButton(
                                 child: Text("Não"),
@@ -255,7 +344,8 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
                         ).then((exceed) {
                           if (exceed == true) {
                             setState(() {
-                              workoutPlans[days[selectedDayIndex]]!.add(exercise);
+                              workoutPlans[days[selectedDayIndex]]!
+                                  .add(exercise);
                             });
                           }
                         });
@@ -271,18 +361,26 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
                         color: const Color.fromARGB(255, 0, 204, 255),
                         child: Column(
                           children: [
-                            Text("Treino ${days[selectedDayIndex]}", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                            Text(
+                              "Treino ${days[selectedDayIndex]}",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
                             Expanded(
                               child: ListView.builder(
-                                itemCount: workoutPlans[days[selectedDayIndex]]!.length,
+                                itemCount:
+                                    workoutPlans[days[selectedDayIndex]]!
+                                        .length,
                                 itemBuilder: (context, index) {
                                   return ListTile(
-                                    title: Text(workoutPlans[days[selectedDayIndex]]![index]),
+                                    title: Text(workoutPlans[days[
+                                            selectedDayIndex]]![index]),
                                     trailing: IconButton(
                                       icon: Icon(Icons.delete, color: Colors.red),
                                       onPressed: () {
                                         setState(() {
-                                          workoutPlans[days[selectedDayIndex]]!.removeAt(index);
+                                          workoutPlans[days[selectedDayIndex]]!
+                                              .removeAt(index);
                                         });
                                       },
                                     ),
@@ -301,9 +399,20 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _addNewExercise,
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(right: 16.0, bottom: 16.0),
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            backgroundColor: Colors.white,
+          ),
+          onPressed: _addNewExercise,
+          icon: Icon(Icons.add, color: Colors.blue),
+          label: Text(
+            "Adicionar exercício",
+            style: TextStyle(fontSize: 16.0, color: Colors.black),
+          ),
+        ),
       ),
     );
   }
